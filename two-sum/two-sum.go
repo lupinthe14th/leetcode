394096b5ea2f2583222ad533a3e88c6debb4da73
hideo-suzuki <hideo-suzuki@iij.ad.jp>
2019-09-12 18:34:04 +0900

    fix(two-sum): Time Limit Exceeded

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,14 +1,9 @@
 package main
 
-import "log"
-
 func twoSumBruteForce(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
-			log.Printf("i: %d, nums[i]: %v\n", i, nums[i])
-			log.Printf("j: %d, nums[j]: %v\n", j, nums[j])
 			sum := nums[i] + nums[j]
-			log.Printf("sum: %d", sum)
 			if sum == target {
 				return []int{i, j}
 			}
