95c8fbbac7d2deb4b98fce192044d43ac37a9002
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-14 00:32:36 +0900

    feat(reverse-integer): first submit version

--- reverse-integer/reverse-integer.go
+++ reverse-integer/reverse-integer.go
@@ -1,41 +1,17 @@
 package reverseinteger
 
-import (
-	"log"
-	"strconv"
-	"strings"
-)
-
 func reverse(x int) int {
-	log.Printf("x: %d", x)
-	integerList := strings.Split(strconv.Itoa(x), "")
-	log.Printf("integerList : %+v", integerList)
-	tmp := make(map[int]string, len(integerList))
-	log.Printf("tmp: %v", tmp)
-	i := len(integerList)
-	log.Printf("len: %d", i)
-	if x > 0 {
-		for i > 0 {
-			log.Printf("i: %d", i)
-			for j := 0; j > len(integerList); j++ {
-				tmp[len(integerList)-i] = integerList[i]
-			}
-			i--
-		}
+	r := x
+	if x < 0 {
+		r = r * -1
 	}
-	log.Printf("after append tmp: %+v", tmp)
-	s := strings.Join(values(tmp), "")
-	num, err := strconv.Atoi(s)
-	if err != nil {
-		log.Fatal(err)
+	reversedNumber := 0
+	for i := 0; i < 3; i++ {
+		reversedNumber = reversedNumber*10 + r%10
+		r /= 10
 	}
-	return num
-}
-
-func values(m map[int]string) []string {
-	s := []string{}
-	for _, v := range m {
-		s = append(s, v)
+	if x < 0 {
+		return reversedNumber * -1
 	}
-	return s
+	return reversedNumber
 }
