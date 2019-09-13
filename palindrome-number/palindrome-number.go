9fa1e99c0dfe2954e3a139ee9da536f57cafa239
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-13 15:39:41 +0900

    improve(palindrome-number): update comment

--- palindrome-number/palindrome-number.go
+++ palindrome-number/palindrome-number.go
@@ -1,9 +1,11 @@
 package palindromenumber
 
 func isPalindrome(x int) bool {
-	// when x < 0, x is not a palindrome.
+	// Special cases:
+	// As discussed avove, when x < 0, x is not a palindrome.
 	// Also if the last digit of the number is 0, in order to be a palindrome,
 	// the first digit of the number also needs to be 0.
+	// Only 0 satisfy this property.
 	if x < 0 || (x%10 == 0 && x != 0) {
 		return false
 	}
@@ -13,5 +15,11 @@ func isPalindrome(x int) bool {
 		revertedNumber = revertedNumber*10 + x%10
 		x /= 10
 	}
+
+	// When the length is an odd number, we can get rid of the middle dighit by
+	// revertedNumber/10
+	// For example when the input is 12321, at the end of the while loop we get
+	// x = 12, revertedNumber = 123, since the middle dighit doesn't matter in
+	// palidrome (it will always equal to itself), we can simply get rid of it.
 	return x == revertedNumber || x == revertedNumber/10
 }
