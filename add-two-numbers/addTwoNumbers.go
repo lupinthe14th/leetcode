ec167941ca6a12cc1619696878be92d5c5ae806e
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-13 18:00:07 +0900

    feat(add-two-number): WIP

--- add-two-numbers/addTwoNumbers.go
+++ add-two-numbers/addTwoNumbers.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"log"
+)
+
 // ListNode is singly-linked list.
 type ListNode struct {
-	val  int
+	Val  int
 	Next *ListNode
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	log.Printf("1: %d", (l1.Val + l2.Val))
+	log.Printf("Next: %d", l1.Next)
+	log.Printf("Next: Val %d", l1.Next.Val)
+
+	return &ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: nil}}}
 }
