74eb8ae30ab332c375feabc2d7cdf2bf2312d779
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-15 17:29:34 +0900

    feat(Maximum Number of Balloons): first submit version

---
+++ contest/weekly-contest-154/problems/maxNumberOfBalloons/maxNumberOfBalloons_test.go
@@ -0,0 +1,26 @@
+package maxnumberofballoons
+
+import (
+	"fmt"
+	"testing"
+)
+
+var cases = []struct {
+	input string
+	want  int
+}{
+	{input: "nlaebolko", want: 1},
+	{input: "loonbalxballpoon", want: 2},
+	{input: "leetcode", want: 0},
+}
+
+func TestHelloWorld(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(fmt.Sprintln(tt.input), func(t *testing.T) {
+			actual := maxNumberOfBalloons(tt.input)
+			if actual != tt.want {
+				t.Errorf("%d, want: %d", actual, tt.want)
+			}
+		})
+	}
+}
