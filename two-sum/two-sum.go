abe87e2a5aa81cad24343b1c72eb2dc2d8bf3f28
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-15 11:35:03 +0900

    wip(two-sum): use hash table

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,5 +1,7 @@
 package twosum
 
+import "log"
+
 func twoSumBruteForce(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
@@ -7,6 +9,25 @@ func twoSumBruteForce(nums []int, target int) []int {
 			if sum == target {
 				return []int{i, j}
 			}
+		}
+	}
+	return []int{}
+}
+
+func twoSumHashTable(nums []int, target int) []int {
+	numsMap := make(map[int]int)
+	// In the first iteration, we add each element's value and its index to the table.
+	for i, num := range nums {
+		numsMap[i] = num
+	}
+	log.Printf("numsMap: %#v", numsMap)
+	// In the second iteration we check if each element's complement (target−nums[i]) exists in the table.
+	// Beware that the complement must not be nums[i] itself!
+	for i, num := range nums {
+		complement := target - num
+		log.Printf("complement: %d", complement)
+		if numsMap[complement] != i {
+			return []int{nums[i]}
 		}
 	}
 	return []int{}
