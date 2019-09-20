a21d0cb7e297051294cad00ced6201d466d40f8d
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-20 18:12:46 +0900

    feat(longest-common-prefix): first submit version

---
+++ longest-common-prefix/longestCommonPrefix_test.go
@@ -0,0 +1,26 @@
+package longestcommonprefix
+
+import (
+	"fmt"
+	"testing"
+)
+
+var cases = []struct {
+	id    int
+	input []string
+	want  string
+}{
+	{id: 1, input: []string{"flower", "flow", "flight"}, want: "fl"},
+	{id: 2, input: []string{"dog", "racecar", "car"}, want: ""},
+}
+
+func TestLongestCommonPrefix(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
+			got := longestCommonPrefix(tt.input)
+			if got != tt.want {
+				t.Errorf("%s, want: %s", got, tt.want)
+			}
+		})
+	}
+}
