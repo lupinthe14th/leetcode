c42b72f025b63603d3f13dfdfae4dcab1bb811c1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-22 00:01:23 +0900

    fix(how-many-apples-can-you-put-into-the-basket): second submit version

--- contest/biweekly-contest-9/problems/problems/maxnumberofapples/maxnumberofapples.go
+++ contest/biweekly-contest-9/problems/problems/maxnumberofapples/maxnumberofapples.go
@@ -1,11 +1,13 @@
 package maxnumberofapples
 
-import "log"
+import (
+	"sort"
+)
 
 func maxNumberOfApples(arr []int) int {
 	var sum int
+	sort.Ints(arr)
 	for i, w := range arr {
-		log.Print(i)
 		sum = sum + w
 		if sum > 5000 {
 			return i
