ffbfe660ff33a1ba4622f44fb2d2ecf12fb90590
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-24 08:11:09 +0900

    feat(ugly-number-iii): first submit version

---
+++ contest/weekly-contest-155/problems/uglynumberiii/uglynumberiii_test.go
@@ -0,0 +1,28 @@
+package nthuglynumber
+
+import (
+	"fmt"
+	"testing"
+)
+
+var cases = []struct {
+	id         int
+	n, a, b, c int
+	want       int
+}{
+	{id: 1, n: 3, a: 2, b: 3, c: 5, want: 4},
+	{id: 2, n: 4, a: 2, b: 3, c: 4, want: 6},
+	{id: 3, n: 5, a: 2, b: 11, c: 13, want: 10},
+	{id: 4, n: 1000000000, a: 2, b: 217983653, c: 336916467, want: 1999999984},
+}
+
+func TestNthUglyNumber(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
+			got := nthUglyNumber(tt.n, tt.a, tt.b, tt.c)
+			if got != tt.want {
+				t.Errorf("%d, want: %d", got, tt.want)
+			}
+		})
+	}
+}
