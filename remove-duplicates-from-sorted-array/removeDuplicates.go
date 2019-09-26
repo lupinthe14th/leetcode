10a8508d1873373fde24832fc7f2203770daa6b4
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-26 17:46:58 +0900

    fix(remove-duplicates-from-sorted-array): fix first submit version

--- remove-duplicates-from-sorted-array/removeDuplicates.go
+++ remove-duplicates-from-sorted-array/removeDuplicates.go
@@ -1,9 +1,16 @@
 package removeduplicatesfromsortedarray
 
 func removeDuplicates(nums []int) int {
-	m := make(map[int]int, 0)
-	for _, n := range nums {
-		m[n]++
+	seen := make(map[int]bool)
+	for i, n := range nums {
+		if i == 0 {
+			nums = nums[:0]
+		}
+		if !seen[n] {
+			seen[n] = true
+			nums = append(nums, n)
+		}
 	}
-	return len(m)
+	nums = nums[:len(nums)]
+	return len(nums)
 }
