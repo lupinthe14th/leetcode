5ea1f6e3a3f04047c4add3e30b79362d035b50ec
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-29 11:54:19 +0900

    fix(5205. Unique Number of Occurrences): tweak

--- contest/weekly-contest-156/problems/uniqueOccurrences/uniqueOccurrences.go
+++ contest/weekly-contest-156/problems/uniqueOccurrences/uniqueOccurrences.go
@@ -1,13 +1,10 @@
 package uniqueoccurrences
 
-import "log"
-
 func uniqueOccurrences(arr []int) bool {
 	var counter = make(map[int]int, 0)
 	for _, v := range arr {
 		counter[v]++
 	}
-	log.Print(counter)
 
 	seen := make(map[int]bool)
 
