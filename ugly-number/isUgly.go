af9fd6e55a7470424a9c7acc640a378f1ecfc672
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-10-01 15:50:53 +0900

    fix(ugly-number): tweak

--- ugly-number/isUgly.go
+++ ugly-number/isUgly.go
@@ -1,26 +1,21 @@
 package isugly
 
-import "log"
-
 func isUgly(num int) bool {
 	for num%2 == 0 {
 		num = num / 2
 	}
-	log.Print(num)
 	if num == 1 {
 		return true
 	}
 	for num%3 == 0 {
 		num = num / 3
 	}
-	log.Print(num)
 	if num == 1 {
 		return true
 	}
 	for num%5 == 0 {
 		num = num / 5
 	}
-	log.Print(num)
 	if num == 1 {
 		return true
 	}
