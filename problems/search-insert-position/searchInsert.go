1b78c68a8172bd3307c4dce9e792046e88e9fb6a
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-10-02 21:35:58 +0900

    improve(search-insert-position): second submit version
    refactoring code

--- problems/search-insert-position/searchInsert.go
+++ problems/search-insert-position/searchInsert.go
@@ -1,16 +1,8 @@
 package searchinsertposition
 
 func searchInsert(nums []int, target int) int {
-
 	for i, n := range nums {
-		if target < n {
-			if i == 0 {
-				return 0
-			}
-			return i
-
-		}
-		if target == n {
+		if target <= n {
 			return i
 		}
 	}
