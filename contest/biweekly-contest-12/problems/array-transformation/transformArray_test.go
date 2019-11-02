0d2e1006d126a24e21522d39504d55d0c191419d
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-03 01:33:09 +0900

    fix(array-transformation): refactor

--- contest/biweekly-contest-12/problems/array-transformation/transformArray_test.go
+++ contest/biweekly-contest-12/problems/array-transformation/transformArray_test.go
@@ -13,6 +13,7 @@ var cases = []struct {
 }{
 	{id: 1, input: []int{6, 2, 3, 4}, want: []int{6, 3, 3, 4}},
 	{id: 2, input: []int{1, 6, 3, 4, 3, 5}, want: []int{1, 4, 4, 4, 4, 5}},
+	{id: 3, input: []int{2, 1, 2, 1, 1, 2, 2, 1}, want: []int{2, 2, 1, 1, 1, 2, 2, 1}},
 }
 
 func TestTransformArray(t *testing.T) {
