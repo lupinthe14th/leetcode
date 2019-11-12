8cf30e11bc984b9638b835ffe5f97d9205d2dfa5
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-12 23:50:21 +0900

    feat(count-and-say): first submit version

--- problems/count-and-say/countAndSay.go
+++ problems/count-and-say/countAndSay.go
@@ -1,7 +1,6 @@
 package conntandsay
 
 import (
-	"log"
 	"strconv"
 	"strings"
 )
@@ -10,29 +9,29 @@ func countAndSay(n int) string {
 	if n == 1 {
 		return strconv.Itoa(1)
 	}
-	count := 0
-	var stack = make([]string, 0)
+	stack := make([]string, 0, n-1)
 	if n > 1 {
 		s := countAndSay(n - 1)
-		i := 0
-		for _, v := range s[1:] {
-			log.Printf("i: %d", s[i])
-			log.Printf("j: %d", v)
-			if string(s[i]) == string(v) {
-				count++
-			}
+		if len(s) == 1 {
+			stack = append(stack, strconv.Itoa(len(s)))
+			stack = append(stack, string(s[len(s)-1]))
+		} else {
+			count := 1
+			for i := 0; i < len(s)-1; i++ {
+				if string(s[i]) == string(s[i+1]) {
+					count++
+				} else {
+					stack = append(stack, strconv.Itoa(count))
+					stack = append(stack, string(s[i]))
+					count = 1
+				}
+				if i == len(s)-2 {
+					stack = append(stack, strconv.Itoa(count))
+					stack = append(stack, string(s[i+1]))
+				}
 
-			if count != 1 {
-				stack = append(stack, string(s[i]))
-				stack = append(stack, strconv.Itoa(count))
-				count = 0
-			} else {
-				stack = append(stack, strconv.Itoa(count))
-				stack = append(stack, string(s[i]))
 			}
-
 		}
 	}
-	log.Print(stack)
 	return strings.Join(stack, "")
 }
--- problems/count-and-say/countAndSay_test.go
+++ problems/count-and-say/countAndSay_test.go
@@ -14,13 +14,18 @@ var cases = []struct {
 	{id: 2, input: 2, want: "11"},
 	{id: 3, input: 3, want: "21"},
 	{id: 4, input: 4, want: "1211"},
+	{id: 5, input: 5, want: "111221"},
+	{id: 6, input: 6, want: "312211"},
+	{id: 7, input: 7, want: "13112221"},
+	{id: 8, input: 8, want: "1113213211"},
+	{id: 9, input: 9, want: "31131211131221"},
+	{id: 10, input: 10, want: "13211311123113112211"},
 }
 
 func TestCountAndSay(t *testing.T) {
 	for _, tt := range cases {
 		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
 			got := countAndSay(tt.input)
-			t.Log(got)
 			if got != tt.want {
 				t.Errorf("%s, want: %s", got, tt.want)
 			}
