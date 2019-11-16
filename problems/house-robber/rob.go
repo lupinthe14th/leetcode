cddb49d51cdbb2722470469823191ddd9a99161c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-16 18:33:13 +0900

    fix(House Robber): remove log

--- problems/house-robber/rob.go
+++ problems/house-robber/rob.go
@@ -1,22 +1,17 @@
 package houserobber
 
-import "log"
-
 func rob(nums []int) int {
 	n := len(nums)
 	dp := make([]int, 2)
 
 	for j := 0; j < 2; j++ {
-		log.Printf("j: %d", j)
 		i := j
 		dp[i] = nums[i]
 		for i < n-2 {
-			log.Printf("i: %d", i)
 			dp[j] = dp[j] + nums[i+2]
 			i = i + 2
 		}
 	}
-	log.Printf("dp: %v", dp)
 	maxMoney := max(dp[0], dp[1])
 	return maxMoney
 }
