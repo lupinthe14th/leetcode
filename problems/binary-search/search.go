6219259067d5fb4f7f8a4e856494fcd38f01d2cd
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-21 22:02:48 +0900

    improve(binary-search): refarence solution

--- problems/binary-search/search.go
+++ problems/binary-search/search.go
@@ -1,15 +1,15 @@
 package binarysearch
 
 func search(nums []int, target int) int {
-	lo, hi := 0, len(nums)
-	for lo < hi {
+	lo, hi := 0, len(nums)-1
+	for lo <= hi {
 		mi := (hi + lo) / 2
 		if nums[mi] == target {
 			return mi
-		} else if nums[mi] < target {
-			lo = mi + 1
+		} else if nums[mi] > target {
+			hi = mi - 1
 		} else {
-			hi = mi
+			lo = mi + 1
 		}
 	}
 	return -1
