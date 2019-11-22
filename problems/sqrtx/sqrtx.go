8407427deb626e4d9b83fea67cac4e21810afc94
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-22 13:34:33 +0900

    feat(sqrtx): first submit version

---
+++ problems/sqrtx/sqrtx.go
@@ -0,0 +1,26 @@
+package sqrtx
+
+import (
+	"math"
+)
+
+func mySqrt(x int) int {
+	if x < 4 {
+		return 1
+	}
+	lo, hi := 0, x+1
+
+	var mi int
+	for lo < hi {
+		mi = (hi + lo) / 2
+		r := math.Pow(float64(mi), 2)
+		if int(r) == x {
+			return mi
+		} else if int(r) < x {
+			lo = mi + 1
+		} else {
+			hi = mi - 1
+		}
+	}
+	return mi
+}
