127e1b21c07935a3bd284214e32d9289bfc5c158
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-26 20:54:31 +0900

    feat(two-sum-ii-input-array-is-sorted): elegant solution version

--- problems/two-sum-ii-input-array-is-sorted/twoSum.go
+++ problems/two-sum-ii-input-array-is-sorted/twoSum.go
@@ -1,29 +1,18 @@
 package twosumiiinputarrayissorted
 
 func twoSum(numbers []int, target int) []int {
-	lo, hi := 0, len(numbers)
+	lo, hi := 0, len(numbers)-1
 	ans := make([]int, 2)
-	var mi int
-	if numbers[hi-1] <= target || numbers[lo] <= target {
-		mi = hi
-	} else {
-		for lo < hi {
-			mi = (lo + hi) / 2
-			if numbers[mi] < target && numbers[mi+1] > target {
-				break
-			} else if numbers[mi] < target {
-				lo = mi
-			} else if numbers[mi] > target {
-				hi = mi
-			}
-		}
-	}
-	for i := 0; i < len(numbers[:mi])+1; i++ {
-		for j := i + 1; j < len(numbers[:mi]); j++ {
-			if numbers[i]+numbers[j] == target {
-				return []int{i + 1, j + 1}
-
-			}
+	for lo < hi {
+		tmp := numbers[lo] + numbers[hi]
+		if tmp == target {
+			ans[0] = lo + 1
+			ans[1] = hi + 1
+			return ans
+		} else if tmp < target {
+			lo++
+		} else {
+			hi--
 		}
 	}
 	return ans
