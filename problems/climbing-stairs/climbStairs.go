675422f2f8220214e3a907ade563a4491741b8b9
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-06 23:26:34 +0900

    improve(climbing-stairs): use math/big version
    Important: Cannot be used with leetcode.

--- problems/climbing-stairs/climbStairs.go
+++ problems/climbing-stairs/climbStairs.go
@@ -1,18 +1,25 @@
 package climbingstairs
 
-func climbStairs(n int) int {
+import "math/big"
 
-	memo := make(map[int]int, n)
+func climbStairs(n int) *big.Int {
+
+	memo := make(map[int]*big.Int, n)
 
 	return climbingstairs(n, memo)
 }
 
-func climbingstairs(n int, memo map[int]int) int {
-	if n == 0 || n == 1 {
-		return 1
+func climbingstairs(n int, memo map[int]*big.Int) *big.Int {
+	if n < 0 {
+		return big.NewInt(0)
 	}
+	if n == 0 || n == 1 {
+		return big.NewInt(1)
+	}
+	var ans big.Int
 	if _, ok := memo[n]; !ok {
-		memo[n] = climbingstairs(n-1, memo) + climbingstairs(n-2, memo)
+		ans.Add(climbingstairs(n-1, memo), climbingstairs(n-2, memo))
+		memo[n] = &ans
 	}
 	return memo[n]
 }
--- problems/climbing-stairs/climbStairs_test.go
+++ problems/climbing-stairs/climbStairs_test.go
@@ -2,28 +2,37 @@ package climbingstairs
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 )
+
+var ansS1610 = "2136010781928948523844645283815574730385064008311995665747822755414246012661413902243436621593742528786413698527404458901613575702930324526113218162028403832301251481141629314362342496588380089335310516958008652225841325082508336642452960414578069346373381958171043406948199684066855095094569350148579342051690860548547807426339114290114"
+var ans1610, _ = new(big.Int).SetString(ansS1610, 10)
 
 var cases = []struct {
 	id    int
 	input int
-	want  int
+	want  *big.Int
 }{
-	{id: 1, input: 1, want: 1},
-	{id: 2, input: 2, want: 2},
-	{id: 3, input: 3, want: 3},
-	{id: 4, input: 4, want: 5},
-	{id: 5, input: 5, want: 8},
-	{id: 6, input: 44, want: 1134903170},
+	{id: 1, input: -1, want: big.NewInt(0)},
+	{id: 2, input: 0, want: big.NewInt(1)},
+	{id: 3, input: 1, want: big.NewInt(1)},
+	{id: 4, input: 2, want: big.NewInt(2)},
+	{id: 5, input: 3, want: big.NewInt(3)},
+	{id: 6, input: 4, want: big.NewInt(5)},
+	{id: 7, input: 5, want: big.NewInt(8)},
+	{id: 8, input: 44, want: big.NewInt(1134903170)},
+	{id: 9, input: 45, want: big.NewInt(1836311903)},
+	{id: 10, input: 46, want: big.NewInt(2971215073)},
+	{id: 11, input: 1610, want: ans1610},
 }
 
 func TestClimbStairs(t *testing.T) {
 	for _, tt := range cases {
 		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
 			got := climbStairs(tt.input)
-			if got != tt.want {
-				t.Errorf("%d, want: %d", got, tt.want)
+			if got.Cmp(tt.want) != 0 {
+				t.Errorf("%v, want: %v", got, tt.want)
 			}
 		})
 	}
