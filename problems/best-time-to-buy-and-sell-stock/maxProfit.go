a346dee4752fdf72821496e634b777a028729c39
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-12 14:29:57 +0900

    improve(best-time-to-buy-and-sell-stock): refarence solution

--- problems/best-time-to-buy-and-sell-stock/maxProfit.go
+++ problems/best-time-to-buy-and-sell-stock/maxProfit.go
@@ -1,23 +1,25 @@
 package besttimetobuyandsellstock
 
 func maxProfit(price []int) int {
-	var a int
+	// 一番安い値を探す。
+	// 安い値に更新されたら次の日（i）に移動する。
+	// 安い値に更新されなかったら、最大利益を計算する。
+	// 利益が前の利益より大きければ利益を更新する。
+	// それを最後の日まで繰り返せば、最終的に最大の利益が得られる。
+	const MaxUint = ^uint(0)
+	const MaxInt = int(MaxUint >> 1)
 
-	for i := 0; i < len(price); i++ {
-		for j := i + 1; j < len(price); j++ {
-			profit := price[j] - price[i]
-			if profit > 0 {
-				a = max(a, profit)
-			}
+	min := MaxInt
+	profit := 0
+	for _, p := range price {
+		if p < min {
+			min = p
+			continue
 		}
 
+		if p-min > profit {
+			profit = p - min
+		}
 	}
-	return a
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return profit
 }
