0304bdd460a4351cd814eb62647489d8fc179795
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-12 15:44:20 +0900

    improve(best-time-to-buy-and-sell-stock): refactor code. update comment

--- problems/best-time-to-buy-and-sell-stock/maxProfit.go
+++ problems/best-time-to-buy-and-sell-stock/maxProfit.go
@@ -1,11 +1,11 @@
 package besttimetobuyandsellstock
 
 func maxProfit(price []int) int {
-	// 一番安い値を探す。
-	// 安い値に更新されたら次の日（i）に移動する。
-	// 安い値に更新されなかったら、最大利益を計算する。
-	// 利益が前の利益より大きければ利益を更新する。
-	// それを最後の日まで繰り返せば、最終的に最大の利益が得られる。
+	// 1. Find the lowest price.（一番安い値を探す。）
+	// 2. Move to the next day after updating the min parameter to the lowest price.（安い値に更新されたら次の日に移動する。）
+	// 3. If the minimum price is not updated, calculate the maximum profit.（安い値に更新されなかったら、最大利益を計算する。）
+	// 4. If the profit has increased, update the profit.（利益が前の利益より大きければ利益を更新する。）
+	// 5. If you repeat it until the last day, you will ultimately get the most profit.（それを最後の日まで繰り返せば、最終的に最大の利益が得られる。）
 	const MaxUint = ^uint(0)
 	const MaxInt = int(MaxUint >> 1)
 
