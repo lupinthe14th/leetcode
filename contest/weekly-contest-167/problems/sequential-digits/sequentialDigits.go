5a26ecc1578e7b4efd07e3b571f4f6432e36f225
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-15 14:28:27 +0900

    feat(weekly-contest-167/sequential-digits): accepted submit version
    ## result
    time over, but I can self-solve this problem

--- contest/weekly-contest-167/problems/sequential-digits/sequentialDigits.go
+++ contest/weekly-contest-167/problems/sequential-digits/sequentialDigits.go
@@ -6,11 +6,10 @@ import (
 
 func sequentialDigits(low int, high int) []int {
 
-	var digit, maxdigit, n int
+	var digit, maxdigit int
 	var ans []int
 	l, h := low, high
 	for l > 0 {
-		n = l % 10
 		l /= 10
 		digit++
 
@@ -20,17 +19,15 @@ func sequentialDigits(low int, high int) []int {
 		maxdigit++
 	}
 	for d := digit; d <= maxdigit; d++ {
-		for i := n; i < 9; i++ {
+		for i := 1; i < 9; i++ {
 			var tmp string
 			tmp += strconv.Itoa(i)
-			for j := 0; j < d-1; j++ {
-				tmp += strconv.Itoa(i + (j + 1))
+			for j := 1; j < d; j++ {
+				tmp += strconv.Itoa(i + j)
 			}
 			c, _ := strconv.Atoi(tmp)
-			if c >= low && c <= high {
+			if len(tmp) == d && c >= low && c <= high {
 				ans = append(ans, c)
-			} else {
-				break
 			}
 		}
 	}
