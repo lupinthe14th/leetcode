33554b826079bab0382a0191905a00dfe218a73d
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-18 16:12:29 +0900

    feat(subarray-sum-equals-k): brute force solution

--- problems/subarray-sum-equals-k/subarraySum.go
+++ problems/subarray-sum-equals-k/subarraySum.go
@@ -1,28 +1,17 @@
 package subarraysumequalsk
 
 func subarraySum(nums []int, k int) int {
-	// 0 - len(nums)までの合計をハッシュにする
-	l := len(nums)
-	dp := make(map[int]int, l)
-	dp[0] = nums[0]
-
-	for i := 1; i < l; i++ {
-		dp[i] = dp[i-1] + nums[i]
-	}
-
 	var c int
-	const MaxUint = ^uint(0)
-	const MaxInt = int(MaxUint >> 1)
+	l := len(nums)
+
 	for i := 0; i < l; i++ {
-		for j := 0; j < l; j++ {
-			sum := MaxInt
-			if i == j {
-				sum = dp[i]
-			} else if i < j {
-				// sum(i,j)=sum(0,j)-sum(0,i), where sum(i,j) represents the sum of all the elements from index i to j-1.
-				sum = dp[j] - dp[i]
-			}
-			if k == sum {
+		sum := nums[i]
+		if sum == k {
+			c++
+		}
+		for j := i + 1; j < l; j++ {
+			sum += nums[j]
+			if sum == k {
 				c++
 			}
 		}
