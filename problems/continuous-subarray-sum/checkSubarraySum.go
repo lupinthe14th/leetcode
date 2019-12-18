d0f8a194ba50e1ff516934fd6dfb26218e0358cc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-19 07:34:51 +0900

    feat(continuous-subarray-sum): I can self-solve this problem
    Algorithms: brute force

---
+++ problems/continuous-subarray-sum/checkSubarraySum.go
@@ -0,0 +1,19 @@
+package continuoussubarraysum
+
+// Brute force solution
+func checkSubarraySum(nums []int, k int) bool {
+	l := len(nums)
+	var sum int
+	for i := 0; i < l; i++ {
+		sum = nums[i]
+		for j := i + 1; j < l; j++ {
+			sum += nums[j]
+			if sum == k {
+				return true
+			} else if k != 0 && sum%k == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ problems/continuous-subarray-sum/checkSubarraySum_test.go
@@ -0,0 +1,32 @@
+package continuoussubarraysum
+
+import (
+	"fmt"
+	"testing"
+)
+
+type input struct {
+	nums []int
+	k    int
+}
+
+var cases = []struct {
+	id    int
+	input input
+	want  bool
+}{
+	{id: 1, input: input{nums: []int{23, 2, 4, 6, 7}, k: 6}, want: true},
+	{id: 2, input: input{nums: []int{23, 2, 6, 4, 7}, k: 6}, want: true},
+	{id: 3, input: input{nums: []int{23, 2, 4, 6, 7}, k: 0}, want: false},
+}
+
+func TestCheckSubarraySum(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(fmt.Sprint(tt.id), func(t *testing.T) {
+			got := checkSubarraySum(tt.input.nums, tt.input.k)
+			if got != tt.want {
+				t.Errorf("%t, want: %t", got, tt.want)
+			}
+		})
+	}
+}
