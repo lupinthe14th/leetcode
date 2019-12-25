836d4c93bebaebb5b0424c7f3e06e81d8aebed57
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-25 15:01:18 +0900

    improve(range-sum-query-immutable): dp(cummulative sum) approach

--- problems/range-sum-query-immutable/solution.go
+++ problems/range-sum-query-immutable/solution.go
@@ -7,15 +7,15 @@ type NumArray struct {
 
 // Constructor is ...
 func Constructor(nums []int) NumArray {
-	return NumArray{nums: nums}
+	l := len(nums)
+	sum := make([]int, l+1)
+	for i := 0; i < l; i++ {
+		sum[i+1] = sum[i] + nums[i]
+	}
+	return NumArray{nums: sum}
 }
 
 // SumRange is given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
 func (na *NumArray) SumRange(i int, j int) int {
-
-	var ans int
-	for i := i; i <= j; i++ {
-		ans += na.nums[i]
-	}
-	return ans
+	return na.nums[j+1] - na.nums[i]
 }
