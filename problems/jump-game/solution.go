f995801d1795303bdf1cb39b8424c04cef2d6f30
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-31 12:56:00 +0900

    improve(jump-game): Approach 2: Dynamic Programming Top-down
    ### result
    Accepted. However, it took 87s on the local machine.

--- problems/jump-game/solution.go
+++ problems/jump-game/solution.go
@@ -1,31 +1,53 @@
 package main
 
+// Index is the status of each position in the array
+type Index int
+
+const (
+	Unknown Index = iota
+	Good
+	Bad
+)
+
 // canJump is determined if you are able to reach the last index.
-// Algorthmus: recursive backtrack
+// Algorthmus: Dynamic Programing Top-down
 func canJump(nums []int) bool {
-	return canJumpFromPosition(0, nums)
+	// Initialize memoization table
+	l := len(nums)
+	memo := make([]Index, l)
+	for i := range nums {
+		memo[i] = Unknown
+	}
+	// 最終地点はGood
+	memo[l-1] = Good
+
+	return canJumpFromPosition(0, nums, memo)
 }
 
-func canJumpFromPosition(p int, nums []int) bool {
-	l := len(nums)
-	// ポジションが最終地点に到達していればtrueを返す
-	if p == l-1 {
-		return true
+func canJumpFromPosition(p int, nums []int, memo []Index) bool {
+	// memoization table check
+	if memo[p] != Unknown {
+		if memo[p] == Good {
+			return true
+		}
+		return false
 	}
 
 	// 最も遠いジャンプ地点を決める
 	// スライスにおける現在地点とその値を足した値が最も遠いジャンプ地点。
 	// もしそれがスライス長を超えるならスライス長が最も遠いジャンプ地点
 	// となるため両者を比較して小さい方を最も遠いジャンプ地点となる。
-	fj := min(p+nums[p], l-1) //  furthest Jump
+	fj := min(p+nums[p], len(nums)-1) //  furthest Jump
 
 	// 次の地点を逐次再帰的に調べる。
 	// npを最大値から最小値までチェック
 	for np := fj; np > p; np-- {
-		if canJumpFromPosition(np, nums) {
+		if canJumpFromPosition(np, nums, memo) {
+			memo[p] = Good
 			return true
 		}
 	}
+	memo[p] = Bad
 	return false
 }
 
