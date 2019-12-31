9cf95a7400b0a7c995f41f66d9957ce76315c175
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-12-31 14:14:49 +0900

    improve(jump-game): Approach 4: Greedy
    ### result
    Accepted.

--- problems/jump-game/solution.go
+++ problems/jump-game/solution.go
@@ -1,48 +1,18 @@
 package main
 
-// Index is the status of each position in the array
-type index int
-
-const (
-	unknown index = iota
-	good
-	bad
-)
-
 // canJump is determined if you are able to reach the last index.
-// Algorthmus: Dynamic Programing Bottom-up
+// Algorthmus: Greedy
 func canJump(nums []int) bool {
-	// Initialize memoization table
+	// Iterating right-to-left, for each position we check if there is a potential jump that reaches a GOOD index.
 	l := len(nums)
-	memo := make([]index, l)
-	for i := range nums {
-		memo[i] = unknown
-	}
-	// 最終地点はGood
-	memo[l-1] = good
-
-	// iを最終地点の一つ前から開始点0までループ
-	for i := l - 2; i >= 0; i-- {
-		// furthest Jump position
-		fj := min(i+nums[i], l-1)
-		// current positionからfurthest jump positionまでチェック
-		// 既にGoodフラグが立っていればbreak
-		for j := i + 1; j <= fj; j++ {
-			if memo[j] == good {
-				memo[i] = good
-				break
-			}
+	gp := l - 1
+	for i := l - 1; i >= 0; i-- {
+		if nums[i]+i >= gp {
+			gp = i
 		}
-	}
-	// 最初の地点でGoodフラグとなっているか確認
-	return memo[0] == good
-}
 
-func min(x, y int) int {
-	if x < y {
-		return x
 	}
-	return y
+	return gp == 0
 }
 
 func main() {}
