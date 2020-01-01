7127e6a85f688f75f8c689b2ef61eeca4a950193
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-01 13:13:52 +0900

    improve(biweekly-contest-16/sum-of-mutated-array-closest-to-target): refactor code

--- contest/biweekly-contest-16/problems/sum-of-mutated-array-closest-to-target/solution.go
+++ contest/biweekly-contest-16/problems/sum-of-mutated-array-closest-to-target/solution.go
@@ -4,20 +4,26 @@ package main
 // Given an integer array arr and a target value target, return the integer value such that when we change all the integers larger than value in the given array to be equal to value, the sum of the array gets as close as possible (in absolute difference) to target.
 // In case of a tie, return the minimum such integer.
 // Notice that the answer is not neccesarilly a number from arr.
-// SeeAlso: https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/discuss/463222/Java-Binary-search-O(nlogk)-k-is-the-max-value-in-arr
+// SeeAlso: - https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/discuss/463222/Java-Binary-search-O(nlogk)-k-is-the-max-value-in-arr
+//          - https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/discuss/463268/JavaC++-4ms-binary-search-short-readable-code-+-sorting-solution
+// 探している値は1から与えられた配列の最大値の間です。探索を高速化するために二分探索を使います。
+//
 func findBestValue(arr []int, target int) int {
 	max := maxA(arr)
 
 	lo, hi := 0, max
 
-	d := target // diif
+	d := target // diff
 	v := max    // prev value
 
 	for lo <= hi {
 		mid := (lo + hi) / 2
 		sum := sumA(arr, mid)
-		cd := abs(target - sum)
+		cd := abs(target - sum) // current diff
 
+		// current diffとdiffの比較
+		// current diffが小さければ最小のdiffを切り替え、探している値vもmidに切り替えます。
+		// current diffと同じであれば、vはmidが小さければ切り替えます。
 		if cd < d {
 			d = cd
 			v = mid
@@ -25,6 +31,8 @@ func findBestValue(arr []int, target int) int {
 			v = min(v, mid)
 		}
 
+		// 合計がターゲットより大きければhiを下げます
+		// そうでなければ、loをあげます
 		if sum > target {
 			hi = mid - 1
 		} else {
@@ -36,7 +44,6 @@ func findBestValue(arr []int, target int) int {
 
 // min is returned the minimum value of x y.
 func min(x, y int) int {
-	// return x&((x-y)>>31) | y&(^(x-y)>>31)
 	if x < y {
 		return x
 	}
@@ -45,7 +52,6 @@ func min(x, y int) int {
 
 // abs is returned the absolute value of x.
 func abs(x int) int {
-	// return (x ^ (x >> 31)) - (x >> 31)
 	if x < 0 {
 		return x * -1
 	}
@@ -56,9 +62,7 @@ func abs(x int) int {
 func sumA(arr []int, n int) int {
 	var sum int
 	for _, v := range arr {
-		if v > n {
-			sum += v
-		}
+		sum += min(v, n)
 	}
 	return sum
 }
