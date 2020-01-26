1e39418e2103313d12e23022160928b5b6da2aa3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-26 20:14:42 +0900

    improve(weekly-contest-173/filter-restaurants-by-vegan-friendly-price-and-distance): refacotor code

--- contest/weekly-contest-173/filter-restaurants-by-vegan-friendly-price-and-distance/solution.go
+++ contest/weekly-contest-173/filter-restaurants-by-vegan-friendly-price-and-distance/solution.go
@@ -4,38 +4,27 @@ import (
 	"sort"
 )
 
+// refarence: https://leetcode.com/problems/filter-restaurants-by-vegan-friendly-price-and-distance/discuss/490311/Python-Straight-Forward
+// Complexity:
+// - Time O(nlogn)
+// - Space O(n)
 func filterRestaurants(restaurants [][]int, veganFriendly int, maxPrice int, maxDistance int) []int {
-	type result struct {
-		id, rating int
-	}
-
-	results := make([]result, 0)
-	for _, restaurant := range restaurants {
-		id := restaurant[0]
-		rating := restaurant[1]
-		vegan := restaurant[2]
-		price := restaurant[3]
-		distance := restaurant[4]
-		if veganFriendly == 1 {
-			if vegan == veganFriendly && price <= maxPrice && distance <= maxDistance {
-				results = append(results, result{id: id, rating: rating})
-			}
-		} else {
-			if price <= maxPrice && distance <= maxDistance {
-				results = append(results, result{id: id, rating: rating})
-			}
+	sort.Slice(restaurants, func(i, j int) bool {
+		if restaurants[i][1] == restaurants[j][1] {
+			return restaurants[i][0] > restaurants[j][0]
 		}
-	}
-	sort.Slice(results, func(i, j int) bool {
-		if results[i].rating == results[j].rating {
-			return results[i].id > results[j].id
-		}
-		return results[i].rating > results[j].rating
+		return restaurants[i][1] > restaurants[j][1]
 	})
 
-	ans := make([]int, len(results))
-	for i, v := range results {
-		ans[i] = v.id
+	ans := make([]int, 0)
+	for _, restaurant := range restaurants {
+		i := restaurant[0]
+		v := restaurant[2]
+		p := restaurant[3]
+		d := restaurant[4]
+		if v >= veganFriendly && p <= maxPrice && d <= maxDistance {
+			ans = append(ans, i)
+		}
 	}
 	return ans
 }
