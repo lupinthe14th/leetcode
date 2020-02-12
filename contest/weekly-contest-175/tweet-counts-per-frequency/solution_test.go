1d687539d229a8f25b1af965f25e1de9320d8c83
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-02-12 14:28:00 +0900

    improve(weekly-contest-175/tweet-counts-per-frequency): refactor code

--- contest/weekly-contest-175/tweet-counts-per-frequency/solution_test.go
+++ contest/weekly-contest-175/tweet-counts-per-frequency/solution_test.go
@@ -8,16 +8,10 @@ import (
 
 func TestTweetSample(t *testing.T) {
 	tweetCounts := Constructor()
-	tweetCounts.RecordTweet("tweet3", 0)
-	tweetCounts.RecordTweet("tweet3", 60)
-	tweetCounts.RecordTweet("tweet3", 10)
-	tweetCounts.RecordTweet("tweet3", 120)
-	tweetCounts.RecordTweet("tweet3", 60*60*10)
 
 	type input struct {
-		freq               string
-		tweetName          string
-		startTime, endTime int
+		action, freq, tweetName  string
+		time, startTime, endTime int
 	}
 
 	type Case struct {
@@ -25,16 +19,27 @@ func TestTweetSample(t *testing.T) {
 		want  []int
 	}
 	var cases = []Case{
-		{input: input{freq: "minute", tweetName: "tweet3", startTime: 0, endTime: 59}, want: []int{2}},
-		{input: input{freq: "minute", tweetName: "tweet3", startTime: 0, endTime: 60}, want: []int{2, 1}},
-		{input: input{freq: "hour", tweetName: "tweet3", startTime: 0, endTime: 210}, want: []int{4}},
-		{input: input{freq: "day", tweetName: "tweet3", startTime: 0, endTime: 60 * 60 * 21}, want: []int{5}},
+		{input: input{action: "get", freq: "minute", tweetName: "tweet3", startTime: 0, endTime: 59}, want: []int{}},
+		{input: input{action: "record", tweetName: "tweet3", time: 0}},
+		{input: input{action: "record", tweetName: "tweet3", time: 60}},
+		{input: input{action: "record", tweetName: "tweet3", time: 10}},
+		{input: input{action: "get", freq: "minute", tweetName: "tweet3", startTime: 0, endTime: 59}, want: []int{2}},
+		{input: input{action: "get", freq: "minute", tweetName: "tweet3", startTime: 0, endTime: 60}, want: []int{2, 1}},
+		{input: input{action: "record", tweetName: "tweet3", time: 120}},
+		{input: input{action: "record", tweetName: "tweet3", time: 60 * 60 * 10}},
+		{input: input{action: "get", freq: "hour", tweetName: "tweet3", startTime: 0, endTime: 210}, want: []int{4}},
+		{input: input{action: "get", freq: "day", tweetName: "tweet3", startTime: 0, endTime: 60 * 60 * 21}, want: []int{5}},
 	}
 	for i, tt := range cases {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			got := tweetCounts.GetTweetCountsPerFrequency(tt.input.freq, tt.input.tweetName, tt.input.startTime, tt.input.endTime)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("%v, want: %v", got, tt.want)
+			switch tt.input.action {
+			case "record":
+				tweetCounts.RecordTweet(tt.input.tweetName, tt.input.time)
+			case "get":
+				got := tweetCounts.GetTweetCountsPerFrequency(tt.input.freq, tt.input.tweetName, tt.input.startTime, tt.input.endTime)
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("%v, want: %v", got, tt.want)
+				}
 			}
 		})
 	}
