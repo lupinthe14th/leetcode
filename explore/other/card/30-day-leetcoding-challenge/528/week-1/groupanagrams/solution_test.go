db7cbb38f4f7b417c50ab3041a81a887e914a612
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-04-09 22:36:47 +0900

    Revert "Group Anagrams: improve refactor test code"
    This reverts commit 517b17bf561911066ff29c8467894a45cdecb0e8.

--- explore/other/card/30-day-leetcoding-challenge/528/week-1/groupanagrams/solution_test.go
+++ explore/other/card/30-day-leetcoding-challenge/528/week-1/groupanagrams/solution_test.go
@@ -2,19 +2,11 @@ package groupanagrams
 
 import (
 	"fmt"
-	"reflect"
 	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
-
-func lessStrings(x, y []string) bool {
-	sort.Strings(x)
-	sort.Strings(y)
-	return reflect.DeepEqual(x, y)
-}
 
 func TestGroupAnagrams(t *testing.T) {
 	tests := []struct {
@@ -23,11 +15,22 @@ func TestGroupAnagrams(t *testing.T) {
 	}{
 		{in: []string{"eat", "tea", "tan", "ate", "nat", "bat"}, want: [][]string{{"ate", "eat", "tea"}, {"nat", "tan"}, {"bat"}}},
 	}
+	// This Transformer sorts a [][]string.
+	trans := cmp.Transformer("Sort", func(in [][]string) [][]string {
+		out := append([][]string(nil), in...) // Copy input to avoid mutating it
+		sort.Slice(out, func(i, j int) bool {
+			for _, v := range out {
+				sort.Strings(v)
+			}
+			return out[i][0] < out[j][0]
+		})
+		return out
+	})
 	for i, tt := range tests {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 
 			got := groupAnagrams(tt.in)
-			if !cmp.Equal(got, tt.want, cmpopts.SortSlices(lessStrings)) {
+			if !cmp.Equal(got, tt.want, trans) {
 				t.Fatalf("got: %v, want: %v", got, tt.want)
 			}
 		})
