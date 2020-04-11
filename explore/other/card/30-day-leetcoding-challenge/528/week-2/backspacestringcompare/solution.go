a62d3cf1138a52d149c6774762907a16d7e63c30
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-04-11 16:16:17 +0900

    Backspace String Compare: Approach: Two Pointer
    直感
    
    文字を書くとき、将来何回バックスペースキーを押すかによって、最終的な文字列の一部になるかどうかが決まります。
    
    代わりに、文字列を逆に反復処理すると、何個のバックスペース文字があったかがわかり、結果に文字が含まれているかどうかがわかります。
    
    アルゴリズム
    
    文字列を逆に繰り返し処理します。バックスペース文字がある場合、次の非バックスペース文字はスキップされます。文字がスキップされなかった場合、それは最終的な答えの一部となります。

--- explore/other/card/30-day-leetcoding-challenge/528/week-2/backspacestringcompare/solution.go
+++ explore/other/card/30-day-leetcoding-challenge/528/week-2/backspacestringcompare/solution.go
@@ -1,20 +1,59 @@
 package backspacestringcompare
 
+// Approach: Two Pointer
+//
+// Intuition
+//
+// When writing a character, it may or may not be part of the final string
+// depending on how many backspace keystrokes occur in the future.
+//
+// If instead we iterate though the string in reverce, then we will know how
+// many backspace characters we have seen, and there fore whether the result
+// includes our character.
+//
+// Algorithm
+//
+// Iterate through the string in reverse. If wee see a backspace character, the
+// next non-backspace character is skipped. If a character isn't skipped, it is
+// part of the final answer.
+//
+// See the comments in the code for more details.
 func backspaceCompare(S string, T string) bool {
-	return b(S) == b(T)
-}
-
-func b(str string) string {
-	var ans []rune
-	for _, s := range str {
-		if len(ans) == 0 && s == '#' {
-			continue
+	i, j := len(S)-1, len(T)-1
+	skipS, skipT := 0, 0
+	for i >= 0 || j >= 0 { // While there may be charas in build(S) or build(T)
+		for i >= 0 { // Find position of next possible char in build(S)
+			if string(S[i]) == "#" {
+				skipS++
+				i--
+			} else if skipS > 0 {
+				skipS--
+				i--
+			} else {
+				break
+			}
 		}
-		if s == '#' {
-			ans = ans[:len(ans)-1]
-			continue
+		for j >= 0 { // Find position of next possible char in build(T)
+			if string(T[j]) == "#" {
+				skipT++
+				j--
+			} else if skipT > 0 {
+				skipT--
+				j--
+			} else {
+				break
+			}
 		}
-		ans = append(ans, s)
+		// If two actual characters are different
+		if i >= 0 && j >= 0 && S[i] != T[j] {
+			return false
+		}
+		// If expecting to compare char vs nothing
+		if (i >= 0) != (j >= 0) {
+			return false
+		}
+		i--
+		j--
 	}
-	return string(ans)
+	return true
 }
--- explore/other/card/30-day-leetcoding-challenge/528/week-2/backspacestringcompare/solution_test.go
+++ explore/other/card/30-day-leetcoding-challenge/528/week-2/backspacestringcompare/solution_test.go
@@ -17,12 +17,15 @@ func TestBackspaceCompare(t *testing.T) {
 		{in: in{s: "ab##", t: "c#d#"}, want: true},
 		{in: in{s: "a##c", t: "#a#c"}, want: true},
 		{in: in{s: "a#c", t: "b"}, want: false},
+		{in: in{s: "a#c", t: ""}, want: false},
+		{in: in{s: "xywrrmp", t: "xywrrmu#p"}, want: true},
+		{in: in{s: "bbbextm", t: "bbb#extm"}, want: false},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			got := backspaceCompare(tt.in.s, tt.in.t)
 			if got != tt.want {
-				t.Fatalf("got: %v, want: %v", got, tt.want)
+				t.Fatalf("in: %v, got: %v, want: %v", tt.in, got, tt.want)
 			}
 		})
 	}
