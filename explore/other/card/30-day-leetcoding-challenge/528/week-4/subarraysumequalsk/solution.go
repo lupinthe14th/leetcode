584006da1fdbf80a8ab352ba5444e996517d04a0
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-04-23 11:49:49 +0900

    Subarray Sum Equals K: using cumulative sum without space

--- explore/other/card/30-day-leetcoding-challenge/528/week-4/subarraysumequalsk/solution.go
+++ explore/other/card/30-day-leetcoding-challenge/528/week-4/subarraysumequalsk/solution.go
@@ -3,14 +3,11 @@ package subarraysumequalsk
 func subarraySum(nums []int, k int) int {
 	c := 0
 	l := len(nums)
-	sumA := make([]int, l+1)
-	sumA[0] = 0
-	for i := 1; i < l+1; i++ {
-		sumA[i] = nums[i-1] + sumA[i-1]
-	}
 	for i := 0; i < l; i++ {
-		for j := i + 1; j < l+1; j++ {
-			if sumA[j]-sumA[i] == k {
+		sum := 0
+		for j := i; j < l; j++ {
+			sum += nums[j]
+			if sum == k {
 				c++
 			}
 		}
