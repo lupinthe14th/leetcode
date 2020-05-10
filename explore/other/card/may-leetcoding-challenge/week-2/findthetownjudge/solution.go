e61497dcf8415674259e2de5685c0902fa09e207
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-10 18:13:28 +0900

    Find the Town Judge: refactor code

--- explore/other/card/may-leetcoding-challenge/week-2/findthetownjudge/solution.go
+++ explore/other/card/may-leetcoding-challenge/week-2/findthetownjudge/solution.go
@@ -1,14 +1,13 @@
 package findthetownjudge
 
 func findJudge(N int, trust [][]int) int {
-	me := make([]bool, N+1)
-	you := make([]int, N+1)
+	a := make([]int, N+1)
 	for i := 0; i < len(trust); i++ {
-		me[trust[i][0]] = true
-		you[trust[i][1]]++
+		a[trust[i][0]]--
+		a[trust[i][1]]++
 	}
 	for i := 1; i <= N; i++ {
-		if !me[i] && you[i] == N-1 {
+		if a[i] == N-1 {
 			return i
 		}
 	}
