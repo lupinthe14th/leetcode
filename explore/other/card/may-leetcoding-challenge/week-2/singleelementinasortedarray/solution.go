4bd092c73537c7719879ee658a675501f70a6b2b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-12 21:38:25 +0900

    Single Element in a Sorted Array: refactor code using xor

--- explore/other/card/may-leetcoding-challenge/week-2/singleelementinasortedarray/solution.go
+++ explore/other/card/may-leetcoding-challenge/week-2/singleelementinasortedarray/solution.go
@@ -1,11 +1,12 @@
 package singleelementinasortedarray
 
+// SeeAlso: https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/627786/C++-O(log-n)-time-O(1)-space-or-Simple-and-clean-or-Use-xor-to-keep-track-of-odd-even-pair
 func singleNonDuplicate(nums []int) int {
 	lo, hi := 0, len(nums)-1
 	for lo < hi {
 		mi := (lo + hi) / 2
 
-		if (mi&1 == 0 && nums[mi] == nums[mi+1]) || (mi&1 == 1 && nums[mi] == nums[mi-1]) {
+		if nums[mi] == nums[mi^1] {
 			lo = mi + 1
 		} else {
 			hi = mi
