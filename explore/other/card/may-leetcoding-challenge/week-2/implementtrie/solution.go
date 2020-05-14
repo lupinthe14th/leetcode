d711ea0bf11947657303cc522fb2b9e3268d9911
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-14 21:51:44 +0900

    Implement Trie (Prefix Tree): Prefix tree solution

--- explore/other/card/may-leetcoding-challenge/week-2/implementtrie/solution.go
+++ explore/other/card/may-leetcoding-challenge/week-2/implementtrie/solution.go
@@ -1,34 +1,51 @@
 package implementtrie
 
-import "strings"
+const ALPABET_SIZE = 26
 
 type Trie struct {
-	m map[string]bool
-	s []string
+	root *Node
+}
+
+type Node struct {
+	children [ALPABET_SIZE]*Node
+	eow      bool
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-	return Trie{m: make(map[string]bool), s: make([]string, 0)}
+	return Trie{new(Node)}
 }
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
-	this.m[word] = true
-	this.s = append(this.s, word)
+	probe := this.root
+	for i := 0; i < len(word); i++ {
+		if probe.children[word[i]-'a'] == nil {
+			probe.children[word[i]-'a'] = new(Node)
+		}
+		probe = probe.children[word[i]-'a']
+	}
+	probe.eow = true
 }
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	return this.m[word]
+	probe := this.searchPrefix(word)
+	return probe != nil && probe.eow
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	for _, word := range this.s {
-		if strings.HasPrefix(word, prefix) {
-			return true
+	return this.searchPrefix(prefix) != nil
+}
+
+func (this *Trie) searchPrefix(word string) *Node {
+	probe := this.root
+	for i := 0; i < len(word); i++ {
+		if probe.children[word[i]-'a'] == nil {
+			return nil
 		}
+		probe = probe.children[word[i]-'a']
 	}
-	return false
+	return probe
 }
