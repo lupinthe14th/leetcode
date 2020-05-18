c8d3d607c1ad939f6d13c880c82b886f54fa4509
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-05-18 22:46:45 +0900

    Permutation in String: refactor code

--- explore/other/card/may-leetcoding-challenge/week-3/permutationinstring/solution.go
+++ explore/other/card/may-leetcoding-challenge/week-3/permutationinstring/solution.go
@@ -1,16 +1,11 @@
 package permutationinstring
 
-import (
-	"reflect"
-)
-
 func checkInclusion(s1 string, s2 string) bool {
 	if len(s1) > len(s2) {
 		return false
 	}
 
-	mem1 := make([]int, 26)
-	mem2 := make([]int, 26)
+	var mem1, mem2 [26]int
 
 	for i := range s1 {
 		mem1[s1[i]-'a']++
@@ -18,7 +13,7 @@ func checkInclusion(s1 string, s2 string) bool {
 	}
 
 	for i := 0; i < len(s2)-len(s1)+1; i++ {
-		if reflect.DeepEqual(mem1, mem2) {
+		if mem1 == mem2 {
 			return true
 		}
 
