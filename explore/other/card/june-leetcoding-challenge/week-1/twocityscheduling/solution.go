80972d8f41e2ca6eefb43b133112adf4abc2d4a2
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-04 10:44:44 +0900

    Two City Scheduling: refactor code

--- explore/other/card/june-leetcoding-challenge/week-1/twocityscheduling/solution.go
+++ explore/other/card/june-leetcoding-challenge/week-1/twocityscheduling/solution.go
@@ -6,48 +6,12 @@ import (
 
 func twoCitySchedCost(costs [][]int) int {
 	out := 0
-	l := len(costs)
-	type memo struct {
-		d, i int
-	}
-	m := make([]memo, 0, l)
-	for i, cost := range costs {
-		m = append(m, memo{d: abs(cost[0], cost[1]), i: i})
-	}
-	sort.Slice(m, func(i, j int) bool {
-		return m[i].d > m[j].d
+	sort.Slice(costs, func(i, j int) bool {
+		return costs[i][0]-costs[i][1] < costs[j][0]-costs[j][1]
 	})
 
-	a, b := 0, 0
-	for i := 0; i < l; i++ {
-		if a < l/2 && b < l/2 {
-			out += min(costs[m[i].i][0], costs[m[i].i][1])
-			if costs[m[i].i][0] < costs[m[i].i][1] {
-				a++
-			} else {
-				b++
-			}
-		} else {
-			if a > b {
-				out += costs[m[i].i][1]
-			} else {
-				out += costs[m[i].i][0]
-			}
-		}
+	for i := 0; i < len(costs)/2; i++ {
+		out += costs[i][0] + costs[i+len(costs)/2][1]
 	}
 	return out
-}
-
-func abs(x, y int) int {
-	if x < y {
-		return y - x
-	}
-	return x - y
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
 }
