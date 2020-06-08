5dab488263129a5c3fd69498faf71fe7673ddd8c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-08 10:12:23 +0900

    Coin Change 2: optimize the space solution

--- explore/other/card/june-leetcoding-challenge/week-1/coinchange2/solution.go
+++ explore/other/card/june-leetcoding-challenge/week-1/coinchange2/solution.go
@@ -2,21 +2,13 @@ package coinchange2
 
 // https://leetcode.com/problems/coin-change-2/discuss/99212/Knapsack-problem-Java-solution-with-thinking-process-O(nm)-Time-and-O(m)-Space
 func change(amount int, coins []int) int {
-	dp := make([][]int, len(coins)+1)
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-	}
-	dp[0][0] = 1
+	dp := make([]int, amount+1)
+	dp[0] = 1
 
-	for i := 1; i < len(coins)+1; i++ {
-		dp[i][0] = 1
-		for j := 1; j < amount+1; j++ {
-			if j >= coins[i-1] {
-				dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
-			} else {
-				dp[i][j] = dp[i-1][j]
-			}
+	for _, coin := range coins {
+		for i := coin; i < amount+1; i++ {
+			dp[i] += dp[i-coin]
 		}
 	}
-	return dp[len(coins)][amount]
+	return dp[amount]
 }
