6439d9b3df603acb52b8411d675cffe1b9ed07d1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-09 21:30:03 +0900

    Is Subsequence: binary search solution for follow-up

--- explore/other/card/june-leetcoding-challenge/week-2/issubsequence/solution.go
+++ explore/other/card/june-leetcoding-challenge/week-2/issubsequence/solution.go
@@ -1,29 +1,40 @@
 package issubsequence
 
+// SeeAlso:
+// - https://leetcode.com/problems/is-subsequence/discuss/87302/Binary-search-solution-for-follow-up-with-detailed-comments
+// - https://leetcode.com/problems/is-subsequence/discuss/87292/Swift-solution-Binary-Search
 func isSubsequence(s string, t string) bool {
-	m, n := len(s), len(t)
-
-	dp := make([][]int, m+1)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
+	memo := make(map[byte][]int, 26)
+	for i := range t {
+		memo[t[i]] = append(memo[t[i]], i)
 	}
-	dp[0][0] = 0
 
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if s[i] == t[j] {
-				dp[i+1][j+1] = dp[i][j] + 1
-			} else {
-				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
-			}
+	prev := 0
+	for i := range s {
+		a, ok := memo[s[i]]
+		if !ok {
+			return false
 		}
+		j := bs(prev, a)
+		if j == len(a) {
+			return false
+		}
+		prev = a[j] + 1
 	}
-	return dp[m][n] == m
+	return true
 }
 
-func max(x, y int) int {
-	if x > y {
-		return x
+func bs(n int, a []int) int {
+	lo, hi := 0, len(a)-1
+
+	for lo <= hi {
+		mi := (lo + hi) >> 1
+		if a[mi] < n {
+			lo = mi + 1
+		} else {
+			hi = mi - 1
+		}
+
 	}
-	return y
+	return lo
 }
