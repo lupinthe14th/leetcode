b57af792f9cab7bf9cb3237587a6655dbdb7949c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-14 15:33:25 +0900

    Find the Longest Substring Containing Vowels in Even Counts: seen discuss, bitwise solution

--- contest/biweekly-contest-21/find-the-longest-substring-containing-vowels-in-even-counts/solution.go
+++ contest/biweekly-contest-21/find-the-longest-substring-containing-vowels-in-even-counts/solution.go
@@ -1,35 +1,21 @@
 package findthelongestsubstringcontainingvowelsinevencounts
 
+import "strings"
+
+// https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/discuss/531840/JavaC++Python-One-Pass
 func findTheLongestSubstring(s string) int {
-
-	ans := 0
-	for i := 0; i < len(s); i++ {
-		for j := len(s); j > i; j-- {
-			vowels := make(map[rune]int, 5)
-			vowels['a'] = 0
-			vowels['e'] = 0
-			vowels['i'] = 0
-			vowels['o'] = 0
-			vowels['u'] = 0
-
-			for _, r := range s[i:j] {
-				if r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u' {
-					vowels[r]++
-				}
-			}
-			if count(vowels) {
-				ans = max(ans, len(s[i:j]))
-			}
+	res, cur, n := 0, 0, len(s)
+	seen := make(map[int]int)
+	seen[0] = -1
+	for i := 0; i < n; i++ {
+		cur ^= 1<<uint(strings.IndexByte("aeiou", s[i])) + 1>>1
+		if _, ok := seen[cur]; !ok {
+			seen[cur] = i
 		}
-	}
-	return ans
-}
+		res = max(res, i-seen[cur])
 
-func count(m map[rune]int) bool {
-	if m['a']%2 == 0 && m['e']%2 == 0 && m['i']%2 == 0 && m['o']%2 == 0 && m['u']%2 == 0 {
-		return true
 	}
-	return false
+	return res
 }
 
 func max(x, y int) int {
