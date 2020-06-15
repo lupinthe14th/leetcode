6753c2190cdd8b221447b09232a00c5645ae1a86
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-15 22:15:27 +0900

    Search in a Binary Search Tree: refactor code

--- explore/other/card/june-leetcoding-challenge/week-3/searchinabinarysearchtree/solution.go
+++ explore/other/card/june-leetcoding-challenge/week-3/searchinabinarysearchtree/solution.go
@@ -10,14 +10,11 @@ func searchBST(root *TreeNode, val int) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if root.Val > val && root.Left != nil {
+	if root.Val > val {
 		return searchBST(root.Left, val)
 	}
-	if root.Val < val && root.Right != nil {
+	if root.Val < val {
 		return searchBST(root.Right, val)
-	}
-	if root.Val != val {
-		return nil
 	}
 	return root
 }
