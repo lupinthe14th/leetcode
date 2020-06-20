448733443db48042cbb3a22b4ad7b430730f3d04
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-06-21 00:02:28 +0900

    Permutation Sequence: recursivw dp solution

--- explore/other/card/june-leetcoding-challenge/week-3/permutationsequence/solution.go
+++ explore/other/card/june-leetcoding-challenge/week-3/permutationsequence/solution.go
@@ -1,40 +1,26 @@
 package permutationsequence
 
-import (
-	"sort"
-)
-
-// Refarence: https://stackoverflow.com/questions/30226438/generate-all-permutations-in-go
+// https://leetcode.com/problems/permutation-sequence/discuss/22544/Easy-understand-Most-concise-C++-solution-minimal-memory-required
 func getPermutation(n int, k int) string {
+	var i, j int
+	f := 1
+
 	b := make([]byte, n)
-	for i := range b {
-		b[i] = byte(i) + '1'
-	}
-	var helper func([]byte, int)
-	res := []string{}
 
-	helper = func(b []byte, n int) {
-		if n == 1 {
-			tmp := make([]byte, len(b))
-			copy(tmp, b)
-			res = append(res, string(tmp))
-		} else {
-			for i := 0; i < n; i++ {
-				helper(b, n-1)
-				if n%2 == 1 {
-					tmp := b[i]
-					b[i] = b[n-1]
-					b[n-1] = tmp
-				} else {
-					tmp := b[0]
-					b[0] = b[n-1]
-					b[n-1] = tmp
-				}
-			}
+	for i = 1; i <= n; i++ {
+		f *= i
+		b[i-1] += byte(i) + '0'
+	}
+	k--
+	for i := 0; i < n; i++ {
+		f /= n - i
+		j = i + k/f
+		tmp := b[j]
+		for ; j > i; j-- {
+			b[j] = b[j-1]
 		}
+		k %= f
+		b[i] = tmp
 	}
-	helper(b, n)
-
-	sort.Strings(res)
-	return res[k-1]
+	return string(b)
 }
