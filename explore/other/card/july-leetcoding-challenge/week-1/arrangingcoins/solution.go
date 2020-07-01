042b0d87f38a67e7a68f7b2c8b598396317fba22
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-01 18:08:47 +0900

    Arranging Coins: binary search solution

--- explore/other/card/july-leetcoding-challenge/week-1/arrangingcoins/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-1/arrangingcoins/solution.go
@@ -1,25 +1,18 @@
 package arrangingcoins
 
 func arrangeCoins(n int) int {
-	if n == 0 {
-		return 0
-	}
-	cum := []int{1}
-	for i := 1; cum[i-1] < n; i++ {
-		cum = append(cum, i+1+cum[i-1])
-	}
-	lo, hi := 0, len(cum)-1
-
+	lo, hi := 0, n
 	for lo <= hi {
 		mi := (lo + hi) / 2
-		if cum[mi] == n {
-			return mi + 1
+		cur := mi * (mi + 1) / 2
+		if cur == n {
+			return mi
 		}
-		if cum[mi] > n {
+		if cur > n {
 			hi = mi - 1
 		} else {
 			lo = mi + 1
 		}
 	}
-	return lo
+	return hi
 }
