be63c8cfcd84a69b3ac64f90ae70b9dc65508cbc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-01 21:40:51 +0900

    Lowest Common Ancestor of a Binary Tree: seen solution

---
+++ explore/learn/card/data-structure-tree/conclusion/lowestcommonancestorofabinarytree/solution_test.go
@@ -0,0 +1,37 @@
+package lowestcommonancestorofabinarytree
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestLowestCommonAncestor(t *testing.T) {
+	type in struct {
+		root, p, q *TreeNode
+	}
+	tests := []struct {
+		in   in
+		want *TreeNode
+	}{
+		{in: in{
+			root: &TreeNode{Val: 3, Left: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}}, Right: &TreeNode{Val: 1, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 8}}},
+			p:    &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}},
+			q:    &TreeNode{Val: 1, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 8}}},
+			want: &TreeNode{Val: 3, Left: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}}, Right: &TreeNode{Val: 1, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 8}}}},
+		{in: in{
+			root: &TreeNode{Val: 3, Left: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}}, Right: &TreeNode{Val: 1, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 8}}},
+			p:    &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}},
+			q:    &TreeNode{Val: 4}},
+			want: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}}}}
+	for i, tt := range tests {
+		i, tt := i, tt
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			got := lowestCommonAncestor(tt.in.root, tt.in.p, tt.in.q)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
