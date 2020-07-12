020bcfb93f7c111c27e7bffd11179ec7d167694b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-12 18:35:16 +0900

    Reverse Bits: using bits package

--- explore/other/card/july-leetcoding-challenge/week-2/reversebits/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-2/reversebits/solution.go
@@ -1,15 +1,7 @@
 package reversebits
 
-import (
-	"fmt"
-	"strconv"
-)
+import "math/bits"
 
 func reverseBits(num uint32) uint32 {
-	tmp := make([]rune, 0)
-	for _, r := range fmt.Sprintf("%032b", num) {
-		tmp = append([]rune{r}, tmp...)
-	}
-	i, _ := strconv.ParseUint(string(tmp), 2, 32)
-	return uint32(i)
+	return bits.Reverse32(num)
 }
