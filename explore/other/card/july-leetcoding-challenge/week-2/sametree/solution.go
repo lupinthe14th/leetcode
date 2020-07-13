907052168dade038533daa7cf2ea2594c0825d2d
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-13 21:13:09 +0900

    Same Tree: refactor code

--- explore/other/card/july-leetcoding-challenge/week-2/sametree/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-2/sametree/solution.go
@@ -7,35 +7,14 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-
-	out := true
-	var helper func(p, q *TreeNode)
-
-	helper = func(p, q *TreeNode) {
-		if p == nil && q == nil {
-			return
-		}
-		if p == nil || q == nil {
-			out = false
-			return
-		}
-		if p.Left != nil && q.Left != nil {
-			helper(p.Left, q.Left)
-		} else if p.Left != nil || q.Left != nil {
-			out = false
-			return
-		}
-		if p.Right != nil && q.Right != nil {
-			helper(p.Right, q.Right)
-		} else if p.Right != nil || q.Right != nil {
-			out = false
-			return
-		}
-		if p.Val != q.Val {
-			out = false
-			return
-		}
+	if p == nil && q == nil {
+		return true
 	}
-	helper(p, q)
-	return out
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
