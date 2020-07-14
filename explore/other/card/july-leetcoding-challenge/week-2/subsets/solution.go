c4cd60e1cf371be9b123c1d66a0ab8e48e87ba89
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-14 16:41:18 +0900

    Subsets: Approach 3: Lexicographic (Binary Sorted) Subsets solution

--- explore/other/card/july-leetcoding-challenge/week-2/subsets/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-2/subsets/solution.go
@@ -1,18 +1,16 @@
 package subsets
 
-// Approach 2: Backtracking
+// Approach 3: Lexicographic (Binary Sorted) Subsets
 func subsets(nums []int) [][]int {
 	out := make([][]int, 0)
-
-	var backtrack func(nums, cur []int)
-
-	backtrack = func(nums, cur []int) {
-		out = append(out, append([]int{}, cur...))
+	for s := 0; s < 1<<uint(len(nums)); s++ {
+		tmp := make([]int, 0)
 		for i := 0; i < len(nums); i++ {
-			backtrack(nums[i+1:], append(cur, nums[i]))
+			if s>>uint(i)&1 == 1 {
+				tmp = append(tmp, nums[i])
+			}
 		}
+		out = append(out, tmp)
 	}
-
-	backtrack(nums, nil)
 	return out
 }
