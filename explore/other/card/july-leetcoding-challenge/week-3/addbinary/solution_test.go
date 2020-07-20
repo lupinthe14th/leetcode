176e37f80b3684dc0c3f474ed97970146b4bd96d
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-20 10:08:21 +0900

    Add Binary: enable t.Parallel

--- explore/other/card/july-leetcoding-challenge/week-3/addbinary/solution_test.go
+++ explore/other/card/july-leetcoding-challenge/week-3/addbinary/solution_test.go
@@ -22,7 +22,7 @@ func TestAddBinary(t *testing.T) {
 	for i, tt := range tests {
 		i, tt := i, tt
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			// t.Parallel()
+			t.Parallel()
 			got := addBinary(tt.in.a, tt.in.b)
 			if got != tt.want {
 				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
