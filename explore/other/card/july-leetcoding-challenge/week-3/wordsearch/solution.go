28e1ceeecde076387a039d522d8781fee0071125
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-22 10:14:52 +0900

    Word Search: using hashmap solution
    but tle again :sob

--- explore/other/card/july-leetcoding-challenge/week-3/wordsearch/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-3/wordsearch/solution.go
@@ -1,55 +1,45 @@
 package wordsearch
 
-type TrieNode struct {
-	word     string
-	children [58]*TrieNode
-}
-
 func exist(board [][]byte, word string) bool {
-	out := ""
-	root := &TrieNode{}
 
-	node := root
-	for _, c := range word {
-		if node.children[c-'A'] == nil {
-			node.children[c-'A'] = &TrieNode{}
+	out := false
+	memo := make(map[byte][][2]int, 0)
+
+	for i := range board {
+		for j := range board[i] {
+			k := board[i][j]
+			memo[k] = append(memo[k], [2]int{i, j})
 		}
-		node = node.children[c-'A']
 	}
-	node.word = word
 
-	var dfs func(i, j int, node *TrieNode)
+	var dfs func(i, j, k int)
 
-	dfs = func(i, j int, node *TrieNode) {
-		if i < 0 || j < 0 || i == len(board) || j == len(board[0]) {
+	dfs = func(i, j, k int) {
+		if i < 0 || j < 0 || i == len(board) || j == len(board[0]) || k == len(word) {
 			return
 		}
 
 		c := board[i][j]
 
-		if c == '#' || node.children[c-'A'] == nil {
+		if c != word[k] {
 			return
 		}
 
-		node = node.children[c-'A']
-
-		if node.word != "" {
-			out = node.word
-			node.word = ""
+		if k == len(word)-1 && c == word[k] {
+			out = true
+			return
 		}
 
 		board[i][j] = '#'
-		dfs(i+1, j, node)
-		dfs(i-1, j, node)
-		dfs(i, j+1, node)
-		dfs(i, j-1, node)
+		dfs(i+1, j, k+1)
+		dfs(i-1, j, k+1)
+		dfs(i, j+1, k+1)
+		dfs(i, j-1, k+1)
 		board[i][j] = c
+	}
 
+	for _, idxes := range memo[word[0]] {
+		dfs(idxes[0], idxes[1], 0)
 	}
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[0]); j++ {
-			dfs(i, j, root)
-		}
-	}
-	return out == word
+	return out
 }
