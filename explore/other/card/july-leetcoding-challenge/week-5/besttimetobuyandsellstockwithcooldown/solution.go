8f3bec4bf30628b85d5a1903774f5675fdeda78b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-07-29 22:02:15 +0900

    Best Time to Buy and Sell Stock with Cooldown: refactor code

--- explore/other/card/july-leetcoding-challenge/week-5/besttimetobuyandsellstockwithcooldown/solution.go
+++ explore/other/card/july-leetcoding-challenge/week-5/besttimetobuyandsellstockwithcooldown/solution.go
@@ -3,21 +3,14 @@ package besttimetobuyandsellstockwithcooldown
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75928/Share-my-DP-solution-(By-State-Machine-Thinking)
 func maxProfit(prices []int) int {
 	l := len(prices)
-	if l <= 1 {
-		return 0
+	sold, hold, rest := 0, -1<<31, 0
+	for i := 0; i < l; i++ {
+		prev := sold
+		sold = hold + prices[i]
+		hold = max(hold, rest-prices[i])
+		rest = max(rest, prev)
 	}
-	s0 := make([]int, l)
-	s1 := make([]int, l)
-	s2 := make([]int, l)
-	s1[0] = -prices[0]
-	s0[0] = 0
-	s2[0] = -1 << 31
-	for i := 1; i < l; i++ {
-		s0[i] = max(s0[i-1], s2[i-1])
-		s1[i] = max(s1[i-1], s0[i-1]-prices[i])
-		s2[i] = s1[i-1] + prices[i]
-	}
-	return max(s0[l-1], s2[l-1])
+	return max(sold, rest)
 }
 
 func max(x, y int) int {
