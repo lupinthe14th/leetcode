5f774f0caa7a908e5d4b933e2aecdbc480c94dd1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-08-03 21:06:53 +0900

    Detect Capital: simple solution, using bool slice

---
+++ explore/other/card/august-leetcoding-challenge/week-1/designhashset/solution_test.go
@@ -0,0 +1,44 @@
+package designhashset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSumRange(t *testing.T) {
+	type in struct {
+		act string
+		key int
+	}
+
+	tests := []struct {
+		in   in
+		want bool
+	}{
+		{in: in{act: "add", key: 1}},
+		{in: in{act: "add", key: 2}},
+		{in: in{act: "cont", key: 1}, want: true},
+		{in: in{act: "cont", key: 3}, want: false},
+		{in: in{act: "add", key: 2}},
+		{in: in{act: "cont", key: 2}, want: true},
+		{in: in{act: "rem", key: 2}},
+		{in: in{act: "cont", key: 2}, want: false},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			c := Constructor()
+			switch tt.in.act {
+			case "add":
+				c.Add(tt.in.key)
+			case "cont":
+				got := c.Contains(tt.in.key)
+				if got != tt.want {
+					t.Fatalf("in %v got: %v want: %v", tt.in, got, tt.want)
+				}
+			case "rem":
+				c.Remove(tt.in.key)
+			}
+		})
+	}
+}
