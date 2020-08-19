6133489fe0863be42b89a8f55467af5571eeb9f2
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-08-19 15:00:02 +0900

    Numbers With Same Consecutive Differences: using recursion

---
+++ explore/other/card/august-leetcoding-challenge/week-3/numberswithsameconsecutivedifferences/solution.go
@@ -0,0 +1,36 @@
+package numberswithsameconsecutivedifferences
+
+func numsSameConsecDiff(N int, K int) []int {
+	out := make([]int, 0)
+
+	var helper func(n int, nums []int)
+
+	helper = func(n int, nums []int) {
+		if len(nums) == N {
+			if len(nums) != 1 && nums[0] == 0 {
+				return
+			}
+			num, e := 0, 1
+			for i := N - 1; i >= 0; i-- {
+				num += nums[i] * e
+				e *= 10
+			}
+			out = append(out, num)
+			return
+		}
+		if K == 0 {
+			helper(n, append(nums, n))
+			return
+		}
+		if n+K <= 9 {
+			helper(n+K, append(nums, n+K))
+		}
+		if n-K >= 0 {
+			helper(n-K, append(nums, n-K))
+		}
+	}
+	for i := 0; i <= 9; i++ {
+		helper(i, []int{i})
+	}
+	return out
+}
