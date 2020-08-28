ad5a32e17bb557da5b6af6122bacde59bc1cc2e1
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-08-29 07:15:05 +0900

    Implement Rand10() Using Rand7(): using Utilizing out-of-range samples

---
+++ explore/other/card/august-leetcoding-challenge/week-4/implementrand10usingrand7/solution.go
@@ -0,0 +1,28 @@
+package implementrand10usingrand7
+
+func rand10() int {
+	var a, b, idx int
+	for {
+		a = rand7()
+		b = rand7()
+		idx = b + (a-1)*7
+		if idx <= 40 {
+			return 1 + (idx-1)%10
+		}
+		a = idx - 40
+		b = rand7()
+
+		idx = b + (a-1)*7
+		if idx <= 60 {
+			return 1 + (idx-1)%10
+		}
+
+		a = idx - 60
+		b = rand7()
+
+		idx = b + (a-1)*7
+		if idx <= 20 {
+			return 1 + (idx-1)%10
+		}
+	}
+}
