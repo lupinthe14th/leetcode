204e77c74ef33efa1f1243394c935ed18c16954a
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-10-23 11:30:42 +0900

    Minimum Depth of Binary Tree: seen discuss

---
+++ explore/other/card/october-leetcoding-challenge/week-4/minimumdepthofbinarytree/solution.go
@@ -0,0 +1,30 @@
+package minimumdepthofbinarytree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	if root.Left == nil {
+		return 1 + minDepth(root.Right)
+	}
+	if root.Right == nil {
+		return 1 + minDepth(root.Left)
+	}
+	return 1 + min(minDepth(root.Left), minDepth(root.Right))
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
