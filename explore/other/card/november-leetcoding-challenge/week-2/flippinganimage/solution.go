40fa051bc371c418d0d2502ddaddf6c37a58090b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-11-11 16:21:55 +0900

    Flipping an Image: solution approach

--- explore/other/card/november-leetcoding-challenge/week-2/flippinganimage/solution.go
+++ explore/other/card/november-leetcoding-challenge/week-2/flippinganimage/solution.go
@@ -3,21 +3,9 @@ package flippinganimage
 func flipAndInvertImage(A [][]int) [][]int {
 
 	c := len(A[0])
-	invert := func(a int) int {
-		if a == 1 {
-			return 0
-		}
-		return 1
-	}
 	for i := range A {
-		for j := 0; j < c/2; j++ {
-			A[i][j], A[i][c-j-1] = A[i][c-j-1], A[i][j]
-		}
-	}
-
-	for i := range A {
-		for j := range A[i] {
-			A[i][j] = invert(A[i][j])
+		for j := 0; j < (c+1)/2; j++ {
+			A[i][j], A[i][c-j-1] = A[i][c-j-1]^1, A[i][j]^1
 		}
 	}
 	return A
