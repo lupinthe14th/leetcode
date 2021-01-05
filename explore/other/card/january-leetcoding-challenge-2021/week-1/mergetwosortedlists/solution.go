0dbbb14aea6a9b46f59f93818a7abfb73d4db3fa
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-01-05 22:08:59 +0900

    Merge Two Sorted Lists: I looked at past solutions

---
+++ explore/other/card/january-leetcoding-challenge-2021/week-1/mergetwosortedlists/solution.go
@@ -0,0 +1,27 @@
+package mergetwosortedlists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	ln := &ListNode{}
+	out := ln
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			ln.Next = l1
+			l1 = l1.Next
+		} else {
+			ln.Next = l2
+			l2 = l2.Next
+		}
+		ln = ln.Next
+	}
+	if l1 != nil {
+		ln.Next = l1
+	} else if l2 != nil {
+		ln.Next = l2
+	}
+	return out.Next
+}
