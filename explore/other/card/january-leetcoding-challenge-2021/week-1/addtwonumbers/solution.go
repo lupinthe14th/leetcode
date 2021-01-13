a96d42a8a74452c79ea44c76b2b5188e847e07c9
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-01-13 17:00:44 +0900

    Add Two Numbers: refactor code

--- explore/other/card/january-leetcoding-challenge-2021/week-1/addtwonumbers/solution.go
+++ explore/other/card/january-leetcoding-challenge-2021/week-1/addtwonumbers/solution.go
@@ -8,34 +8,34 @@ type ListNode struct {
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	ln := &ListNode{}
 	out := ln
-	pre := 0
+	carry := 0
 	for l1 != nil && l2 != nil {
 		ln.Next = &ListNode{}
 		ln = ln.Next
-		ln.Val = (pre + l1.Val + l2.Val) % 10
-		ln.Val = (pre + l1.Val + l2.Val) % 10
-		pre = (pre + l1.Val + l2.Val) / 10
+		ln.Val = (carry + l1.Val + l2.Val) % 10
+		ln.Val = (carry + l1.Val + l2.Val) % 10
+		carry = (carry + l1.Val + l2.Val) / 10
 		l1 = l1.Next
 		l2 = l2.Next
 	}
 	for l1 != nil {
 		ln.Next = &ListNode{}
 		ln = ln.Next
-		ln.Val = (pre + l1.Val) % 10
-		pre = (pre + l1.Val) / 10
+		ln.Val = (carry + l1.Val) % 10
+		carry = (carry + l1.Val) / 10
 		l1 = l1.Next
 	}
 	for l2 != nil {
 		ln.Next = &ListNode{}
 		ln = ln.Next
-		ln.Val = (pre + l2.Val) % 10
-		pre = (pre + l2.Val) / 10
+		ln.Val = (carry + l2.Val) % 10
+		carry = (carry + l2.Val) / 10
 		l2 = l2.Next
 	}
-	if pre != 0 {
+	if carry != 0 {
 		ln.Next = &ListNode{}
 		ln = ln.Next
-		ln.Val = pre
+		ln.Val = carry
 	}
 	return out.Next
 }
